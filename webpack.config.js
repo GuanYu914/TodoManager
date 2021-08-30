3d12f03dc7befeff8bc7050a52a8df54745c79e3
Emory <aa774281@gmail.com>
2021-08-30 15:42:08 +0800

    use babel and browserslist

--- webpack.config.js
+++ webpack.config.js
@@ -20,5 +20,17 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
     ],
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   },
 };
