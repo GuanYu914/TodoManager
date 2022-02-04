477ce3b8c489dcc40cf9d14d40223b99e8f97d35
Emory <aa774281@gmail.com>
2022-02-04 23:07:23 +0800

    use double qoutes and add configs of autoprefixer & browserslist plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,31 @@
-const path = require('path');
-const postcssPresetEnv = require('postcss-preset-env');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
-    host: '172.20.186.226', // it will be changed when using wsl2 dev env
+    host: "172.18.134.82", // it will be changed when using wsl2 dev env
     port: 8080,
   },
-  entry: './src/js/index.js',
+  entry: "./src/js/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   performance: {
     maxEntrypointSize: 512000,
-    maxAssetSize: 512000
+    maxAssetSize: 512000,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: [
-          'style-loader',
-          'css-loader',
+          "style-loader",
+          "css-loader",
           {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [postcssPresetEnv],
-              },
-            },
+            loader: "postcss-loader",
           },
         ],
       },
@@ -39,20 +33,20 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          "style-loader",
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader',
+          "sass-loader",
         ],
       },
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
       },
